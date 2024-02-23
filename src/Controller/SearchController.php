9d00288cb034eca602d03dabbef042af72d7d950
Antoine-Welter <Antoine.Welter@etu.univ-amu.fr>
2024-02-23 11:20:18 +0100

    page search via api

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,16 +3,16 @@
 namespace App\Controller;
 
 use App\Api\ApiRequest;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class HomeController extends AbstractController
+class SearchController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/search/{f}', name: 'app_search')]
     public function index(Request $request): Response
     {
         $form = $this->createFormBuilder()
@@ -25,9 +25,16 @@ class HomeController extends AbstractController
             $fruit = $form->get('search')->getData();
             return $this->redirectToRoute('app_search', array('f' => $fruit));
         }
-
-        return $this->render('home/index.html.twig', [
+        $search = $this->searchApi("https://api.discogs.com/database/search?q=".$request->get('f')."&key=".$_ENV['CONSUMER_KEY']."&secret=".$_ENV['CONSUMER_SECRET_KEY']);
+        return $this->render('search/index.html.twig', [
+            'searchs'=>$search['results'],
             'form'=>$form
         ]);
+    }
+
+    private function searchApi(String $url): array
+    {
+        $apiRequest = new ApiRequest();
+        return $apiRequest->apiRequest($url);
     }
 }
