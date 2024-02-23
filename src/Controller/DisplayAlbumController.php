1b006da384611fedf0ecd309ef6e82c688387efa
NeekoInAbsentia <maxime.albertelli@etu.univ-amu.fr>
2024-02-23 13:59:39 +0100

    ajout détail album

--- src/Controller/DisplayAlbumController.php
+++ src/Controller/DisplayAlbumController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
+use App\Entity\Chanson;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DisplayAlbumController extends AbstractController
 {
-    #[Route('/display/album', name: 'app_display_album')]
-    public function index(): Response
+    public function __construct(public EntityManagerInterface $entityManager){
+    }
+    #[Route('/display/album/{id}', name: 'app_display_album')]
+    public function index(Request $request): Response
     {
+        $idAlbum = $request -> get('id');
         return $this->render('display_album/index.html.twig', [
-            'controller_name' => 'DisplayAlbumController',
+            'album' => $this->entityManager->getRepository(Album::class)->find($idAlbum),
         ]);
     }
 }
